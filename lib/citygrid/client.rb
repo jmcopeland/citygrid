9e0a5916aa6040d4cf2cb4ff65458266735828dd
Justin Copeland <copeland.justin@gmail.com>
2011-11-01 20:51:05 -0700

    add API v2 access for 'search by latlon',  'reviews', and 'details'

--- lib/citygrid/client.rb
+++ lib/citygrid/client.rb
@@ -2,7 +2,7 @@ module Citygrid
   
   class Client
     include HTTParty
-    base_uri "http://api.citygridmedia.com/content/places/v2"
+    base_uri "http://api.citygridmedia.com/content/"
     format :json
     
     attr_reader :api_key
@@ -10,30 +10,24 @@ module Citygrid
     def initialize(options={})
       @api_key = options[:api_key] || Citygrid.api_key
     end
+    
+    #    http://docs.citygridmedia.com/display/citygridv2/Places+API
+    def search(options={})
+      mashup(self.class.get("places/v2/search/where", :query => options.merge(self.default_options)))
+    end
 
-    def search_locations(options={})
-      mashup(self.class.get("/search/where", :query => options.merge(self.default_options)))
+    def search_by_latlon(options={})
+      mashup(self.class.get("places/v2/search/latlon", :query => options.merge(self.default_options)))
     end
 
     def detail(options={})
-      mashup(self.class.get("/detail", :query => options.merge(self.default_options)))
+      mashup(self.class.get("places/v2/detail", :query => options.merge(self.default_options)))
     end
-    
-    # def search_events(options={})
-    #   mashup(self.class.get("/search/events", :query => options.merge(self.default_options)))
-    # end
-    
-    # def profile(options={})
-    #   if options[:client_ip].nil?
-    #     puts "Dude, a client_ip is required."
-    #   else
-    #     mashup(self.class.get("/profile/", :query => options.merge(self.default_options)))
-    #   end
-    # end
-    
-    # def reviews(options={})
-    #   mashup(self.class.get("/reviews/", :query => options.merge(self.default_options)))
-    # end
+        
+    #    http://docs.citygridmedia.com/display/citygridv2/Reviews+API
+    def reviews(options={})
+      mashup(self.class.get("reviews/v2/search/where/", :query => options.merge(self.default_options)))
+    end
     
     protected
     
